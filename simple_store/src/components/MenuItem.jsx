c460621ec929fc51986e36fb13809c801ca444e5
zeinab kamkar <zeinabkamkar4@gmail.com>
2022-04-28 20:30:07 +0430

    file and folders added for homepage

---
+++ simple_store/src/components/MenuItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MenuItem extends Component {
+  render() {
+    return (
+      <div>MenuItem</div>
+    )
+  }
+}
+
+export default MenuItem
---
+++ simple_store/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Menu from './../components/Menu';
+
+export class Home extends Component {
+  render() {
+    return (
+        <>
+        <Menu></Menu>
+    </>
+    )
+  }
+}
+
+export default Home;
