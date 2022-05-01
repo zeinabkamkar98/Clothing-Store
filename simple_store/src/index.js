dfc288a00a4685376f35b7c3ac33628400e1581a
zeinab kamkar <zeinabkamkar4@gmail.com>
2022-05-01 21:51:03 +0430

    margin error fixed

--- simple_store/src/index.js
+++ simple_store/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,3 +13,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+reportWebVitals();
