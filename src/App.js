3960fc368df30574f8053a180bc99e569d5cf2aa
zeinab kamkar <zeinabkamkar4@gmail.com>
2022-05-19 14:00:30 +0430

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React from "react";
 
 import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
-import ShopPage from "./pages/ShopPage";
-import ContactPage from "./pages/ContactPage";
-import SignInPage from "./pages/SignInPage";
-import CheckOutPage from "./pages/CheckOutPage";
-import ShopDetailPage from "./pages/ShopDetailPage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "./redux/user/user.selector";
+
+import { CircularProgress } from "@mui/material";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const ShopPage = React.lazy(() => import("./pages/ShopPage"));
+const ContactPage = React.lazy(() => import("./pages/ContactPage"));
+const SignInPage = React.lazy(() => import("./pages/SignInPage"));
+const CheckOutPage = React.lazy(() => import("./pages/CheckOutPage"));
+const ShopDetailPage = React.lazy(() => import("./pages/ShopDetailPage"));
 
 function App() {
   const currentUser = useSelector(selectCurrentUser);
   return (
     <>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/shop" element={<ShopPage />}></Route>
-        <Route path="/contact" element={<ContactPage />}></Route>
-        <Route path="/check-out" element={<CheckOutPage />}></Route>
-        <Route
-          path="/shop-detail/:collectionId"
-          element={<ShopDetailPage />}
-        ></Route>
-        <Route
-          path="/sign-in"
-          element={currentUser ? <Navigate to="/" /> : <SignInPage />}
-        ></Route>
-      </Routes>
+      <React.Suspense fallback={<CircularProgress />}>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/shop" element={<ShopPage />}></Route>
+          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="/check-out" element={<CheckOutPage />}></Route>
+          <Route
+            path="/shop-detail/:collectionId"
+            element={<ShopDetailPage />}
+          ></Route>
+          <Route
+            path="/sign-in"
+            element={currentUser ? <Navigate to="/" /> : <SignInPage />}
+          ></Route>
+        </Routes>
+      </React.Suspense>
     </>
   );
 }
